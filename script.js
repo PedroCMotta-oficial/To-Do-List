c5e60a682ba4fd5dc02b38cc6118969984666bb7
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-01-14 15:33:36 -0300

    masonry layout v1.0
    simple refinements to the lists spectre and the (up to date) 100% completion of the masonry layout for the website

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-let listsCounter = 0;
+// masonry library initialization
+let masonryInstance;
+document.addEventListener('DOMContentLoaded', function () {
+  masonryInstance = new Masonry('#lists-container', {
+    itemSelector: '.list',
+    columnWidth: 500,
+    gutter: 25,
+    fitWidth: true,
+  });
+});
 
-// para botão de criação de listas
+
+// variables made to list creation
+let listsCounter = 0;
 const listsContainer = document.getElementById("lists-container");
 const listToBeCloned = document.getElementById("listBeta");
+
 
 function addTask(button) {
   const targetList = button.closest('.list');
   const inputBox = targetList.querySelector('#input-box');
   const tasksContainer = targetList.querySelector('#tasks-container');
-
 
   if(inputBox.value === '') {
     alert("You must write something!");
@@ -18,14 +29,20 @@ function addTask(button) {
     li.innerHTML = inputBox.value;
     tasksContainer.appendChild(li);
 
-    // making a delete way to the task that was created before
+    // deleting way to the task
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
   inputBox.value = "";
+
+  // masonry update
+  masonryInstance.reloadItems();
+  masonryInstance.layout();
+
   saveData();
 }
+
 
 function addList() {
   listsCounter++;
@@ -33,17 +50,27 @@ function addList() {
   newList.removeAttribute('id');
   newList.setAttribute('data-id', `list-${listsCounter}`);
   listsContainer.appendChild(newList);
+
+  // masonry update
+  masonryInstance.reloadItems();
+  masonryInstance.layout();
+
   saveData();
 }
 
 
-// checagem de tarefas
+// task checking
 document.addEventListener("click", function(e) {
   if(e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
     saveData();
   } else if(e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+
+    // masonry update
+    masonryInstance.reloadItems();
+    masonryInstance.layout();
+
     saveData();
   }
 }, false)
@@ -54,5 +81,11 @@ function saveData() {
 }
 function showTask() {
   listsContainer.innerHTML = localStorage.getItem("data");
+  
+  // layout calc with timeout adjustments
+  setTimeout(function() {
+    masonryInstance.reloadItems();
+    masonryInstance.layout();
+  }, 100)
 }
 showTask();
